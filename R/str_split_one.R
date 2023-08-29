62dbd7da89eca6b38018e5ef0ab678ff42d8f3e9
Omar Jordan Camanto <ocamanto@uwo.ca>
2023-08-29 14:46:03 -0400

    Fixed documentation errors.

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -2,17 +2,16 @@
 #'
 #' @param string A character with, at most, one element
 #' @inheritParams stringr::str_split
-#' @param n
 #'
 #' @return A character vector
 #' @export
 #'
 #' @examples
-#' #' x <- "alfa,bravo,charlie,delta"
+#' x <- "alfa,bravo,charlie,delta"
 #' str_split_one(x, pattern = ",")
 #' str_split_one(x, pattern = ",", n = 2)
 #'
-#' #' y <- "192.168.0.1"
+#' y <- "192.168.0.1"
 #' str_split_one(y, pattern = stringr::fixed("."))
 str_split_one <- function(string, pattern, n = Inf) {
   stopifnot(is.character(string), length(string) <= 1)
